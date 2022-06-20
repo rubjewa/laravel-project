26a786e5515c0b71a245f086738d9aacb0a8e973
rubenw <rubjewa@hotmail.com>
2022-06-20 13:44:47 +0200

    Reworked master and welcome views

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,15 +1,8 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Livewire</title>
-        @livewireStyles
-    </head>
-    <body>
+@extends('layouts.master')
 
-    <livewire:counter />
-      @livewireScripts
-    </body>
-</html>
+@section('content')
+
+<h1> Welkom !</h1>
+
+@endsection
