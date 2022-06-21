00b21b7c530d1d81578222ac6a166f986d962a1c
rubenw <rubjewa@hotmail.com>
2022-06-21 17:39:57 +0200

    Update PostSeeder and posts-table

--- database/migrations/2022_06_20_082933_create_posts_table.php
+++ database/migrations/2022_06_20_082933_create_posts_table.php
@@ -15,7 +15,9 @@ return new class extends Migration {
         Schema::create('posts', function (Blueprint $table) {
             $table->id();
             $table->string('title');
-            $table->string('description');
+            $table->text('description');
+            $table->string('image')->nullable();
+            // $table->boolean('featured')->default(0);
             $table->timestamps();
         });
     }
